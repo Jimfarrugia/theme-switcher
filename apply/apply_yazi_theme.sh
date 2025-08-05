10cb01df2f97a64cd8feeeb004db7aa04ae8d5d5
jimfarrugia <the.jim.farrugia@gmail.com>
2025-08-06 04:58:11 +1000

    add script to apply yazi theme

---
+++ apply/apply_yazi_theme.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#
+# Desc: Apply a theme from the themes directory by copying the theme's
+#       files to the yazi config directory.
+#
+# Usage: apply_yazi_theme.sh "theme_name"
+#
+
+set -eo pipefail
+
+YAZI_CONFIG_DIR="$HOME/.config/yazi"
+YAZI_THEMES_DIR="$YAZI_CONFIG_DIR/themes"
+
+THEME_NAME="$1" # eg. tokyo_night
+THEME_DIR="$YAZI_THEMES_DIR/$THEME_NAME"
+
+# Make sure theme name was provided
+if [[ -z $THEME_NAME ]]; then
+  echo -e "\e[31m \e[0mYazi theme name argument wasn't provided."
+  exit 1
+fi
+
+# Make sure the theme exists
+if [[ ! -d "$THEME_DIR" ]]; then
+  echo -e "\e[31m \e[0mYazi theme '$THEME_NAME' not found."
+  exit 1
+fi
+
+# Remove current theme's files from the config directory
+rm -rf "$YAZI_CONFIG_DIR/flavors" \
+  "$YAZI_CONFIG_DIR/theme.tmTheme" "$YAZI_CONFIG_DIR/theme.toml"
+
+# Copy the new theme's files to the config directory
+cp -r "$THEME_DIR/." "$YAZI_CONFIG_DIR/"
+
+echo -e "\e[32m \e[0mYazi theme '$THEME_NAME' applied."
