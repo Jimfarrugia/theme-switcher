e26d77b7b29e746b5d85d25e702b3b0c9dbbfa20
jimfarrugia <the.jim.farrugia@gmail.com>
2025-08-12 14:17:52 +1000

    add several themes

--- apply/apply_imv_theme.sh
+++ apply/apply_imv_theme.sh
@@ -16,6 +16,11 @@ THEME_NAME="$1" # eg. tokyo_night
 declare -A themes
 themes["tokyo_night"]="#1A1B26"
 themes["dracula"]="#282A36"
+themes["gruvbox"]="#282828"
+themes["nord"]="#2E3440"
+themes["catppuccin"]="#1E1E2E"
+themes["rose_pine"]="#191724"
+themes["everforest"]="#272E33"
 
 # Make sure theme name was provided
 if [[ -z $THEME_NAME ]]; then
